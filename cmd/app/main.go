ffbb07f1cb525fc18f59b72fce7f481852f58905
Hunsolo02 <misax.ermakox@gmail.com>
2024-06-03 17:13:20 +0300

    upd

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"newproj/handlers"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	router := chi.NewMux()
+
+	router.Get("/foo", handlers.Make(handlers.HandleFoo))
+
+	listendAddr := os.Getenv("LISTEN_ADDR")
+	slog.Info("HTTP server started", "addr", listendAddr)
+
+	http.ListenAndServe(listendAddr, router)
+
+}
+
+func handleFoo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "foo")
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,11 @@
 module newproj
 
-go 1.18
+go 1.21
 
-require github.com/go-chi/chi/v5 v5.0.12 // indirect
+toolchain go1.21.1
+
+require (
+	github.com/a-h/templ v0.2.707 // indirect
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
